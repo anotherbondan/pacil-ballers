e40a64df22734185fbc37e1c433d530913407bc2
anotherbondan <anandagautama3112@gmail.com>
2025-09-03 17:41:49 +0700

    Initial commit

---
+++ pacil_ballers/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for pacil_ballers project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pacil_ballers.settings')
+
+application = get_wsgi_application()
