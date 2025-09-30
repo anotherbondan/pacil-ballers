492eafbdef908a1b59bd2e64cb50e41bf901e2a4
anotherbondan <anandagautama3112@gmail.com>
2025-10-01 00:22:54 +0700

    Tugas 5

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, show_json, show_json_by_id, show_xml, show_xml_by_id, create_product, show_product, login_user, logout_user, register
+from main.views import show_main, show_json, show_json_by_id, show_xml, show_xml_by_id, create_product, show_product, login_user, logout_user, register, edit_product, delete_product
 
 app_name = 'main'
 
@@ -14,4 +14,6 @@ urlpatterns = [
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
     path('register/', register, name='register'),
+    path('product/<uuid:id>/edit', edit_product, name='edit_product'),
+    path('product/<uuid:id>/delete', delete_product, name='delete_product'),
 ]
